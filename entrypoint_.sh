06af4f7cfb098dd68d5b9153ab09acd5c6e1c0e8
AnnaKolmychek <a.kolmychek@gmail.com>
2024-12-27 14:48:56 +0300

    update deploy

---
+++ entrypoint_.sh
@@ -0,0 +1,19 @@
+#!/bin/env
+echo "Making migrations..."
+python manage.py makemigrations
+echo "Running migrations..."
+python manage.py migrate
+#echo "Importing test data..."
+#python manage.py import_test_data
+
+
+#mkdir -p /app/static_backend
+echo "Collect static files..."
+python manage.py collectstatic --noinput
+#echo "Copy static files..."
+#cp -r /app/staticfiles/. /app/static_backend/
+
+# Start gunicorn and daphne
+echo "Starting gunicorn..."
+gunicorn --bind 0.0.0.0:9000 --workers 3 config.wsgi:application 0
+exec "$@"
