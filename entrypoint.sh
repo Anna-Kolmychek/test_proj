2f6fa4226de5ece0482a0b0c1aa94a7c3ef94c9e
AnnaKolmychek <a.kolmychek@gmail.com>
2024-12-27 15:36:34 +0300

    update deploy

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,13 +7,12 @@ python manage.py migrate
 #python manage.py import_test_data
 
 
-#mkdir -p /app/static_backend
+mkdir -p /app/backend_static
 echo "Collect static files..."
 python manage.py collectstatic --noinput
-#echo "Copy static files..."
-#cp -r /app/staticfiles/. /app/static_backend/
-
+echo "Copy static files..."
+cp -r /app/staticfiles/. /app/backend_static/
 # Start gunicorn and daphne
 echo "Starting gunicorn..."
-gunicorn --bind 0.0.0.0:9000 --workers 3 config.wsgi:application 0
+gunicorn --bind 0.0.0.0:9000 config.wsgi:application 0
 exec "$@"
